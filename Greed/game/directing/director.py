3ed35da5ce7d8f93203d6056227658262fd5ce36
Clancy Thomas <clancyvthomas@gmail.com>
2022-06-09 08:43:00 -0600

    Updated gems/rocks to move down and relocated the player

--- Greed/game/directing/director.py
+++ Greed/game/directing/director.py
@@ -60,6 +60,7 @@ class Director:
         robot.move_next(max_x, max_y)
         
         for artifact in artifacts:
+            artifact.move_next(max_x, max_y)
             if robot.get_position().equals(artifact.get_position()):
                 message = artifact.get_message()
                 banner.set_text(message)    
